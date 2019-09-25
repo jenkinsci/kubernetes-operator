c119cb7bc458b55421b05e0cd618b3c9fecce6b2
Jakub Al-Khalili <jakubalkhalili@gmail.com>
2019-09-25 12:36:23 +0200

    #104 Add EXCLUDE_CONFIG_XML env variable

--- backup/pvc/bin/backup.sh
+++ backup/pvc/bin/backup.sh
@@ -5,11 +5,18 @@ set -eo pipefail
 [[ ! $# -eq 1 ]] && echo "Usage: $0 backup_number" && exit 1;
 [[ -z "${BACKUP_DIR}" ]] && echo "Required 'BACKUP_DIR' env not set" && exit 1;
 [[ -z "${JENKINS_HOME}" ]] && echo "Required 'JENKINS_HOME' env not set" && exit 1;
+[[ -z "${EXCLUDE_CONFIG_XML}" ]] && EXCLUDE_CONFIG_XML=true;
 
 backup_number=$1
 echo "Running backup"
 
-tar -C ${JENKINS_HOME} -czf "${BACKUP_DIR}/${backup_number}.tar.gz" --exclude jobs/*/config.xml --exclude jobs/*/workspace* -c jobs
+CONFIG_ARG=""
+
+if [[ ${EXCLUDE_CONFIG_XML} == true ]]; then
+  CONFIG_ARG="--exclude jobs/*/config.xml"
+fi
+
+tar -C ${JENKINS_HOME} -czf "${BACKUP_DIR}/${backup_number}.tar.gz" ${CONFIG_ARG} --exclude jobs/*/workspace* -c jobs
 
 [[ ! -s ${BACKUP_DIR}/${backup_number}.tar.gz ]] && echo "backup file '${BACKUP_DIR}/${backup_number}.tar.gz' is empty" && exit 1;
 
