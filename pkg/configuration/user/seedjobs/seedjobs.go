b548eec55a7bde08a4d89974969df78b7b5df78c
Mateusz Korus <varret@wp.pl>
2021-09-23 15:45:19 +0200

    Add HostAliases to schema

--- pkg/configuration/user/seedjobs/seedjobs.go
+++ pkg/configuration/user/seedjobs/seedjobs.go
@@ -435,6 +435,7 @@ func agentDeployment(jenkins *v1alpha2.Jenkins, namespace string, agentName stri
 					NodeSelector:     jenkins.Spec.Master.NodeSelector,
 					Tolerations:      jenkins.Spec.Master.Tolerations,
 					ImagePullSecrets: jenkins.Spec.Master.ImagePullSecrets,
+					HostAliases:      jenkins.Spec.Master.HostAliases,
 					Containers: []corev1.Container{
 						{
 							Name:  "jnlp",
