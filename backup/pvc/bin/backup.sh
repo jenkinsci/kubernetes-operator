e98748e57b032498eea9fc28d68a4a1496dd5153
Tomas Vopat <tomas.vopat@gmail.com>
2022-09-14 15:30:11 +0300

    Add trap to remove temporary data in backup.sh script
    In case the backup.sh script exits unexpectedly, it leaves the hanging
    resources filling up the disk space.
    
    Added trap that will delete the temporary directory on EXIT.

--- backup/pvc/bin/backup.sh
+++ backup/pvc/bin/backup.sh
@@ -2,24 +2,36 @@
 
 set -eo pipefail
 
-[[ ! $# -eq 1 ]] && echo "Usage: $0 backup_number" && exit 1;
-[[ -z "${BACKUP_DIR}" ]] && echo "Required 'BACKUP_DIR' env not set" && exit 1;
-[[ -z "${JENKINS_HOME}" ]] && echo "Required 'JENKINS_HOME' env not set" && exit 1;
-BACKUP_TMP_DIR=$(mktemp -d)
+[[ ! "${#}" -eq 1 ]] && echo "Usage: ${0} backup_number" >&2 && exit 1;
+[[ -z "${BACKUP_DIR}" ]] && echo "Required 'BACKUP_DIR' env not set" >&2 && exit 1;
+[[ -z "${JENKINS_HOME}" ]] && echo "Required 'JENKINS_HOME' env not set" >&2 && exit 1;
 
-backup_number=$1
+BACKUP_TMP_DIR="$(mktemp -d)"
+trap 'rm -rf "${BACKUP_TMP_DIR}"' EXIT
+
+backup_number="${1}"
 echo "Running backup"
 
 # config.xml in a job directory is a config file that shouldnt be backed up
 # config.xml in child directores is state that should. For example-
 # branches/myorg/branches/myrepo/branches/master/config.xml should be retained while
 # branches/myorg/config.xml should not
-tar -C ${JENKINS_HOME} -czf "${BACKUP_TMP_DIR}/${backup_number}.tar.gz" --exclude jobs/*/workspace* --no-wildcards-match-slash --anchored --exclude jobs/*/config.xml -c jobs && \
-mv ${BACKUP_TMP_DIR}/${backup_number}.tar.gz ${BACKUP_DIR}/${backup_number}.tar.gz
+tar \
+    --directory="${JENKINS_HOME}" \
+    --create \
+    --gzip \
+    --file "${BACKUP_TMP_DIR}/${backup_number}.tar.gz" \
+    --no-wildcards-match-slash \
+    --anchored \
+    --exclude jobs/*/workspace* \
+    --exclude jobs/*/config.xml \
+    jobs
 
-rm -r ${BACKUP_TMP_DIR}
+mv "${BACKUP_TMP_DIR}/${backup_number}.tar.gz" "${BACKUP_DIR}/${backup_number}.tar.gz"
 
-[[ ! -s ${BACKUP_DIR}/${backup_number}.tar.gz ]] && echo "backup file '${BACKUP_DIR}/${backup_number}.tar.gz' is empty" && exit 1;
+if [[ ! -s "${BACKUP_DIR}/${backup_number}.tar.gz" ]] ; then
+    echo "backup file '${BACKUP_DIR}/${backup_number}.tar.gz' is empty" >&2
+    exit 1
+fi
 
 echo Done
-exit 0
