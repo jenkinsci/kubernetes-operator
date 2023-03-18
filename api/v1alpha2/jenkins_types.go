894cc578d1573042b328a14e0d107b191dc2cecd
brokenpip3 <brokenpip3@gmail.com>
2023-03-18 18:30:41 +0100

    fix typo

--- api/v1alpha2/jenkins_types.go
+++ api/v1alpha2/jenkins_types.go
@@ -371,7 +371,7 @@ type JenkinsMaster struct {
 	Plugins []Plugin `json:"plugins,omitempty"`
 
 	// Allow to override jenkins-plugin-cli default behavior
-	// while donwloading the plugin and dependencies
+	// while downloading the plugin and dependencies
 	// see: https://github.com/jenkinsci/plugin-installation-manager-tool#cli-options
 	LatestPlugins bool `json:"latestPlugins"`
 
