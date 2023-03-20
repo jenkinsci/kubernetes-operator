764d340ac4ff4bf35072c90f3f82d6f8068d26c7
brokenpip3 <brokenpip3@gmail.com>
2023-03-20 23:52:07 +0100

    update base plugins

--- pkg/plugins/base_plugins.go
+++ pkg/plugins/base_plugins.go
@@ -3,11 +3,11 @@ package plugins
 const (
 	configurationAsCodePlugin           = "configuration-as-code:1569.vb_72405b_80249"
 	gitPlugin                           = "git:5.0.0"
-	jobDslPlugin                        = "job-dsl:1.81"
-	kubernetesPlugin                    = "kubernetes:3893.v73d36f3b_9103"
+	jobDslPlugin                        = "job-dsl:1.82"
+	kubernetesPlugin                    = "kubernetes:3896.v19b_160fd9589"
 	kubernetesCredentialsProviderPlugin = "kubernetes-credentials-provider:1.209.v862c6e5fb_1ef"
 	workflowAggregatorPlugin            = "workflow-aggregator:596.v8c21c963d92d"
-	workflowJobPlugin                   = "workflow-job:1282.ve6d865025906"
+	workflowJobPlugin                   = "workflow-job:1284.v2fe8ed4573d4"
 )
 
 // basePluginsList contains plugins to install by operator.
