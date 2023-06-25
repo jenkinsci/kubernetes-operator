22a0e676e2f669128bb80a371754d8478471c81c
brokenpip3 <brokenpip3@gmail.com>
2023-06-25 15:45:40 +0200

    fix spelling

--- backup/pvc/bin/get-latest.sh
+++ backup/pvc/bin/get-latest.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 [[ -z "${BACKUP_DIR}" ]] && echo "Required 'BACKUP_DIR' env not set" && exit 1
-# Search for all the tar.* inside the backup dir to support the migration beetwen gzip vs zstd
+# Search for all the tar.* inside the backup dir to support the migration between gzip vs zstd
 latest=$(find ${BACKUP_DIR} -name '*.tar.*' -exec basename {} \; | sort -g | tail -n 1)
 
 if [[ "${latest}" == "" ]]; then
