cfca97144ebf5da4165428848518d667a228b0a3
brokenpip3 <brokenpip3@gmail.com>
2024-02-24 16:28:46 +0100

    fix helm e2e and remove minikube for good

--- variables.mk
+++ variables.mk
@@ -18,13 +18,13 @@ define strip_quotes
 $(shell echo $(1) | sed -e 's/^"//' -e 's/"$$//')
 endef
 
-include config.base.env
-$(foreach var,$(shell cat config.base.env),$(eval $(call strip_quotes,$(var))))
-
 # Import config
 # You can change the default config with `make config="config_special.env" build`
-config ?= config.minikube.env
+config ?= config.kind.env
 include $(config)
+
+include config.base.env
+$(foreach var,$(shell cat config.base.env),$(eval $(call strip_quotes,$(var))))
 
 # Set an output prefix, which is the local directory if not specified
 PREFIX?=$(shell pwd)
