90b00bd6e8fe36bf61d374c9d8c5804206a7b9aa
brokenpip3 <brokenpip3@gmail.com>
2024-06-22 17:17:50 +0200

    feat(backup): log everything stdout/err, implement lockfile for both backup/restore

--- backup/pvc/bin/restore.sh
+++ backup/pvc/bin/restore.sh
@@ -1,29 +1,45 @@
 #!/usr/bin/env bash
 
 set -eo pipefail
+source "$(dirname "$0")/utils.sh"
 
-[[ ! $# -eq 1 ]] && echo "Usage: $0 backup_number" && exit 1
-[[ -z "${BACKUP_DIR}" ]] && echo "Required 'BACKUP_DIR' env not set" && exit 1;
-[[ -z "${JENKINS_HOME}" ]] && echo "Required 'JENKINS_HOME' env not set" && exit 1;
+[[ ! $# -eq 1 ]] && _log "ERROR" "Usage: $0 <backup number>" && exit 1
+[[ -z "${BACKUP_DIR}" ]] && _log "ERROR" "Required 'BACKUP_DIR' env not set" && exit 1
+[[ -z "${JENKINS_HOME}" ]] && _log "ERROR" "Required 'JENKINS_HOME' env not set" && exit 1
+BACKUP_NUMBER=$1
+RETRY_COUNT=${RETRY_COUNT:-3}
+RETRY_INTERVAL=${RETRY_INTERVAL:-60}
 
-backup_number=$1
-backup_file="${BACKUP_DIR}/${backup_number}"
-echo "Running restore backup with backup number #${backup_number}"
+# --> Check if another restore process is running (operator restart/crash)
+TRAP_FILE="${BACKUP_DIR}/_restore_${BACKUP_NUMBER}_is_running"
+trap "rm -f ${TRAP_FILE}" SIGINT SIGTERM
 
-if [[ -f "$backup_file.tar.gz" ]]; then
-    echo "Old format tar.gz found, restoring it"
+for ((i=0; i<RETRY_COUNT; i++)); do
+    [[ ! -f "${TRAP_FILE}" ]] && _log "INFO" "Restore: No other process are running, restoring" && break
+    _log "INFO" "Restore is already running. Waiting for ${RETRY_INTERVAL} seconds..."
+    sleep "${RETRY_INTERVAL}"
+done
+[[ -f "${TRAP_FILE}" ]] && { _log "ERROR" "Restore is stil running after waiting ${RETRY_COUNT} time ${RETRY_INTERVAL}s. Exiting."; exit 1; }
+# --< Done
+
+_log "INFO" "Running restore backup with backup number #${BACKUP_NUMBER}"
+touch "${TRAP_FILE}"
+BACKUP_FILE="${BACKUP_DIR}/${BACKUP_NUMBER}"
+
+if [[ -f "$BACKUP_FILE.tar.gz" ]]; then
+    _log "INFO" "Restore: ld format tar.gz found, restoring it"
     OPTS=""
     EXT="tar.gz"
-elif [[ -f "$backup_file.tar.zstd" ]]; then
-    echo "Backup file found, proceeding"
+elif [[ -f "$BACKUP_FILE.tar.zstd" ]]; then
+    _log "INFO" "Restore: Backup file found, proceeding"
     OPTS="--zstd"
     EXT="tar.zstd"
 else
-  echo "ERR: Backup file not found: $backup_file"
+  _log "ERROR" "Restore: Backup file not found: $BACKUP_FILE"
   exit 1
 fi
 
-tar $OPTS -C "${JENKINS_HOME}" -xf "${BACKUP_DIR}/${backup_number}.${EXT}"
+tar $OPTS -C "${JENKINS_HOME}" -xf "${BACKUP_DIR}/${BACKUP_NUMBER}.${EXT}"
 
-echo Done
+_log "INFO" "Restore: ${BACKUP_NUMBER} Done"
 exit 0
