fd5d09d2b695a107c63758a216008e6554e7b285
brokenpip3 <brokenpip3@gmail.com>
2024-06-22 19:17:11 +0200

    cosmetic log change, in case of error also send it to current process stdout

--- backup/pvc/bin/utils.sh
+++ backup/pvc/bin/utils.sh
@@ -6,8 +6,9 @@ _log() {
     local message="$2"
     local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
     if [[ "$level" =~ ^(ERROR|ERR|error|err)$ ]]; then
-        echo "${timestamp} [${level}] ${message}" > /proc/1/fd/2
+        echo "${timestamp} - ${level} - ${message}" > /proc/1/fd/2
     else
-        echo "${timestamp} [${level}] ${message}" > /proc/1/fd/1
+        echo "${timestamp} - ${level} - ${message}" > /proc/1/fd/1
+        echo "${timestamp} - ${level} - ${message}" >&2
     fi
 }
