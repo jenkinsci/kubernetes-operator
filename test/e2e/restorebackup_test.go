9ba4586ad8bb7efd69680499dbf906a195ba7500
Ansh Garhewal <me@anshdevs.in>
2024-07-21 14:15:59 +0530

    fix: restorebackup test migrated to use  VolumeResourceRequirements

--- test/e2e/restorebackup_test.go
+++ test/e2e/restorebackup_test.go
@@ -54,7 +54,7 @@ func createPVC(namespace string) {
 		},
 		Spec: corev1.PersistentVolumeClaimSpec{
 			AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
-			Resources: corev1.ResourceRequirements{
+			Resources: corev1.VolumeResourceRequirements{
 				Requests: corev1.ResourceList{
 					corev1.ResourceStorage: resource.MustParse("1Gi"),
 				},
