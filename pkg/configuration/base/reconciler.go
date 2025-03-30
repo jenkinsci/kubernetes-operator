3499dcef716430610b400f01f6522d1c50844f18
brokenpip3 <brokenpip3@gmail.com>
2025-03-30 16:15:29 +0200

    Revert "ci(lint): re-enable prealloc in golangci-lint config"
    This reverts commit a889852897036eadf69369d9cd77caa3b3e8f546.

--- pkg/configuration/base/reconciler.go
+++ pkg/configuration/base/reconciler.go
@@ -267,7 +267,7 @@ func compareMap(expected, actual map[string]string) bool {
 }
 
 func compareEnv(expected, actual []corev1.EnvVar) bool {
-	actualEnv := make([]corev1.EnvVar, 0, len(actual))
+	var actualEnv []corev1.EnvVar
 	for _, env := range actual {
 		if env.Name == "KUBERNETES_PORT_443_TCP_ADDR" || env.Name == "KUBERNETES_PORT" ||
 			env.Name == "KUBERNETES_PORT_443_TCP" || env.Name == "KUBERNETES_SERVICE_HOST" {
@@ -292,7 +292,7 @@ func CompareContainerVolumeMounts(expected corev1.Container, actual corev1.Conta
 
 // compareVolumes returns true if Jenkins pod and Jenkins CR volumes are the same
 func (r *JenkinsBaseConfigurationReconciler) compareVolumes(actualPod corev1.Pod) bool {
-	toCompare := make([]corev1.Volume, 0, len(actualPod.Spec.Volumes))
+	var toCompare []corev1.Volume
 	for _, volume := range actualPod.Spec.Volumes {
 		// filter out service account
 		if strings.HasPrefix(volume.Name, actualPod.Spec.ServiceAccountName) {
