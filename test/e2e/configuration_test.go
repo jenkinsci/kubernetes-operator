0e1059265b697e61882f69cfac29c59e6fd8a7a3
brokenpip3 <40476330+brokenpip3@users.noreply.github.com>
2026-04-12 07:37:15 +0000

    chore: update Jenkins lts version to 2.541.3

--- test/e2e/configuration_test.go
+++ test/e2e/configuration_test.go
@@ -26,8 +26,8 @@ const e2e = "e2e"
 // Plugin versions should be the same as in
 // github.com/jenkinsci/kubernetes-operator/pkg/plugins/base_plugins.go
 const (
-	configurationAsCodePlugin           = "configuration-as-code:2037.v8e5349845172"
-	gitPlugin                           = "git:5.10.0"
+	configurationAsCodePlugin           = "configuration-as-code:2069.vb_a_36d7a_570c8"
+	gitPlugin                           = "git:5.10.1"
 	jobDslPlugin                        = "job-dsl:3654.vdf58f53e2d15"
 	kubernetesPlugin                    = "kubernetes:4423.vb_59f230b_ce53"
 	kubernetesCredentialsProviderPlugin = "kubernetes-credentials-provider:1.303.vdfcf47fb_b_fef"
